01be8d2982ff2d6bf408df8a3502e80d5f672b2e
Henry Okonkwo <47309196+henryookonkwo@users.noreply.github.com>
2022-01-07 21:18:57 -0500

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import Entry from "./Entry";
+import emojipedia from "../emojipedia";
+
+function createEntry(detail) {
+  return (
+    <Entry
+      key={detail.id}
+      emoji={detail.emoji}
+      name={detail.name}
+      meaning={detail.meaning}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      {/* <Entry
+        emoji={emojipedia[0].emoji}
+        name={emojipedia[0].name}
+        meaning={emojipedia[0].meaning}
+      /> */}
+
+      {emojipedia.map(createEntry)}
+
+      {/* <dl className="dictionary">
+        <div className="term">
+          <dt>
+            <span className="emoji" role="img" aria-label="Tense Biceps">
+              💪
+            </span>
+            <span>Tense Biceps</span>
+          </dt>
+          <dd>
+            “You can do that!” or “I feel strong!” Arm with tense biceps. Also
+            used in connection with doing sports, e.g. at the gym.
+          </dd>
+        </div>
+
+        <div className="term">
+          <dt>
+            <span className="emoji" role="img" aria-label="Tense Biceps">
+              🙏
+            </span>
+            <span>Person With Folded Hands</span>
+          </dt>
+          <dd>
+            Two hands pressed together. Is currently very introverted, saying a
+            prayer, or hoping for enlightenment. Is also used as a “high five”
+            or to say thank you.
+          </dd>
+        </div>
+        <div className="term">
+          <dt>
+            <span className="emoji" role="img" aria-label="Tense Biceps">
+              🤣
+            </span>
+            <span>Rolling On The Floor, Laughing</span>
+          </dt>
+          <dd>
+            This is funny! A smiley face, rolling on the floor, laughing. The
+            face is laughing boundlessly. The emoji version of “rofl“. Stands
+            for „rolling on the floor, laughing“.
+          </dd>
+        </div>
+      </dl> */}
+    </div>
+  );
+}
+
+export default App;
